a2f3226cd03ab0dfc2dab9c73d7a7b4d1966a78c
Adam Mackey <adambmackey@gmail.com>
2022-06-22 09:58:41 -0400

    counter app

---
+++ src/CounterHooks.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+export default function CounterHooks({ initialCount }) {
+   const [state, setState] = useState({ count: initialCount })
+
+    return (
+        <div>
+            <button onClick={() => setState({count: state.count - 1})}>-</button>
+            <span>{state.count}</span>
+            <button>+</button>
+        </div>
+    )
+}
